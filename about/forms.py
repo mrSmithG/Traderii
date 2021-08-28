6773d9d103001b69f0d33034ef435fa0d41439c0
mrSmithG <gbengamosun@gmail.com>
2021-08-28 21:56:24 +0100

    added the pages app

--- about/forms.py
+++ about/forms.py
@@ -1,10 +1,31 @@
-
 from django import forms
+from django.core.mail import send_mail
+import logging
+
+
+logging = logging.getLogger(__name__) 
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Your name', max_length=100)
     message = forms.CharField(max_length=600, widget=forms.Textarea)
     email = forms.EmailField(label='Your email')
+
+    def send_mail(self):
+        logger.info("Sending mail to customer service")
+        message = "From: {0}\n{1}".format(
+                self.cleaned_data["name"],
+                self.cleaned_data["message"],
+
+        )
+
+        send_mail(
+                "Site message",
+                message,
+                "site@Traderii.domain",
+                ["customerservice@Traderii.domain"],
+                fail_silently=False,
+                )
+
 
     
 
--- about/tests.py
+++ about/tests.py
@@ -1,3 +1,2 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.test import TestCase                  from django.urls import reverse                                                                     # Create your tests here.   
+class TestPage(TestCase):                             def test_home_page_works(self):                       response = self.client.get(reverse("home"))                                                         self.assertEqual(response.status_code, 200)                                                         self.assertTemplateUsed(response, 'homepage.html')                                                  self.assertContains(response, 'Welcome')
