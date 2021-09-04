22d77adf6570feaa096bbc78f2a1de9c45df5e6c
mrSmithG <gbengamosun@gmail.com>
2021-09-04 16:44:56 +0100

    fixed homepage.html error

--- about/tests.py
+++ about/tests.py
@@ -1,2 +1,4 @@
-from django.test import TestCase                  from django.urls import reverse                                                                     # Create your tests here.   
+from django.test import TestCase
+from django.urls import reverse
+
 class TestPage(TestCase):                             def test_home_page_works(self):                       response = self.client.get(reverse("home"))                                                         self.assertEqual(response.status_code, 200)                                                         self.assertTemplateUsed(response, 'homepage.html')                                                  self.assertContains(response, 'Welcome')
