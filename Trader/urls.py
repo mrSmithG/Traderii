513188e75bb6a2c05c42724610690429f96e9186
mrSmithG <gbengamosun@gmail.com>
2021-09-04 16:10:51 +0100

    Deleted some files

--- Trader/urls.py
+++ Trader/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', include('about.urls')),
-    path('pages/', include('pages.urls')),
+    path('', include('about.urls')),
+
 ]
