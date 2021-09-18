34f92d289d5dd8c0fa54f05b9214511bd7ea164b
mrSmithG <gbengamosun@gmail.com>
2021-09-18 02:00:27 +0100

    Tests and signals

--- Trader/settings.py
+++ Trader/settings.py
@@ -16,7 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Templates Directory
-TEMPLATE_DIR = os.path.join(BASE_DIR,"Templates")
+TEMPLATE_DIR = BASE_DIR / 'Templates'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
